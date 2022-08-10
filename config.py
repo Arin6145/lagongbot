57fde2d742f8b4776fb5b109a10a8dcd83c8f303
Arin6145 <108275177+Arin6145@users.noreply.github.com>
2022-08-10 22:15:30 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+Token = "MTAwNjEyNDgwNDM3OTc3OTExMw.GkIoD_.neaBpn6tPu7V0K-_Nh2Mh84bvKjS5fV3UHoPr8"
+Prefix = '='
+PointsName = "라공코인"
+Log_channel = 957627803279450122 # Channel where all shop purchases are logged.
+POINTS_TO_BE_GIVEN_PER_MESSAGE = 2 # Points which will be given to members per message.
